abe1d398ceba80be909ebf6fb2424d2ed65028bf
pipipipipip <43127989+pi12138@users.noreply.github.com>
2019-09-16 16:33:50 +0800

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ def handle_time_list(time_list):
     """
     处理float格式的时间数据为 年/月/日 小时:分钟
     time_list: 时间列表
-    return: 处理好的时间数据迭代器
+    return: 处理好的时间数据列表
     """
     new_time_list = [xlrd.xldate_as_datetime(i, 0).strftime(r'%Y/%m/%d %H:%M') for i in time_list]
     return new_time_list
